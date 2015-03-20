272130c1c0a8e15e41b844c93b27a0d59fe6cc1f
bbklab <bbklab@126.com>
2015-03-20 10:09:43 +0800

    rename directory util as sinfo

--- src/main.go
+++ src/main.go
@@ -8,19 +8,33 @@ import (
 	"inc"
 )
 
-func main() {
-	mo.BindTextdomain(inc.AppName, "./share/locale/")
+func init() {
+	// gettext settings
+	mo.BindTextdomain(inc.AppName, inc.GetAppRealDirPath()+"/share/locale/")
 	mo.Textdomain(inc.AppName)
 	os.Setenv("LANGUAGE", "zh_CN.UTF8")
 	mo.SetLocale(mo.LC_ALL, "zh_CN.UTF8")
+}
 
-	fmt.Println(inc.GetAppRealPath())
-	inc.ShowVersion()
+func main() {
 
 	if os.Geteuid() != 0 {
-		fmt.Println(mo.Gettext("require root privileges!"))
+		output("require root privileges!")
+		os.Exit(1)
+	}
+
+	if inc.GetSysLoadavg() >= 0 {
+		output("system overload!")
 		os.Exit(1)
 	}
 
 	os.Exit(0)
+}
+
+func output(s string) {
+	fmt.Println(trans(s))
+}
+
+func trans(s string) string {
+	return mo.Gettext(s)
 }
