cc50bcfd31b9ce946e2167f31cd1a09bf0f939ed
bbklab <bbklab@126.com>
2015-03-23 11:47:02 +0800

    add var.go

--- src/inc/caller.go
+++ src/inc/caller.go
@@ -4,31 +4,7 @@ import (
 	"os/exec"
 )
 
-var (
-	CheckerDirPath string = GetAppRealDirPath() + "/c"
-)
-
-var Checker map[string]string
-
-func init() {
-	Checker = map[string]string{
-		"cpu":       CheckerDirPath + "/cpu",
-		"dnsbl":     CheckerDirPath + "/dnsbl",
-		"emgmqueue": CheckerDirPath + "/emgmqueue",
-		"emqueue":   CheckerDirPath + "/emqueue",
-		"fsio":      CheckerDirPath + "/fsio",
-		"http":      CheckerDirPath + "/http",
-		"pop":       CheckerDirPath + "/pop",
-		"smtp":      CheckerDirPath + "/smtp",
-		"imap":      CheckerDirPath + "/imap",
-		"mysqlping": CheckerDirPath + "/mysqlping",
-		"mysqlrepl": CheckerDirPath + "/mysqlrepl",
-		"tcpconn":   CheckerDirPath + "/tcpconn",
-		"iostat":    CheckerDirPath + "/iostat",
-	}
-}
-
-func Caller(name, args string) string {
+func Caller(name string, args string) string {
 	cmd := exec.Command(name, args)
 	if output, err := cmd.Output(); err != nil {
 		return ""
---
+++ src/inc/var.go
@@ -0,0 +1,27 @@
+package inc
+
+var (
+	CheckerDirPath string = GetAppRealDirPath() + "/c"
+	SinfoDirPath   string = GetAppRealDirPath() + "/sinfo"
+	Sinfo          string = SinfoDirPath + "/sinfo"
+)
+
+var Checker map[string]string
+
+func init() {
+	Checker = map[string]string{
+		"cpu":       CheckerDirPath + "/cpu",
+		"dnsbl":     CheckerDirPath + "/dnsbl",
+		"emgmqueue": CheckerDirPath + "/emgmqueue",
+		"emqueue":   CheckerDirPath + "/emqueue",
+		"fsio":      CheckerDirPath + "/fsio",
+		"http":      CheckerDirPath + "/http",
+		"pop":       CheckerDirPath + "/pop",
+		"smtp":      CheckerDirPath + "/smtp",
+		"imap":      CheckerDirPath + "/imap",
+		"mysqlping": CheckerDirPath + "/mysqlping",
+		"mysqlrepl": CheckerDirPath + "/mysqlrepl",
+		"tcpconn":   CheckerDirPath + "/tcpconn",
+		"iostat":    CheckerDirPath + "/iostat",
+	}
+}
