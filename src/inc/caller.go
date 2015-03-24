032f35b9e13e60b6f7266e8a0c9d6860934e149d
bbklab <bbklab@126.com>
2015-03-24 16:41:06 +0800

    bugfix on caller.go again

--- src/inc/caller.go
+++ src/inc/caller.go
@@ -4,9 +4,9 @@ import (
 	"os/exec"
 )
 
-func Caller(name string, args string) string {
-	//cmd := exec.Command(name, args)	// this lead to error if args contains space
-	cmd := exec.Command("/bin/sh", "-c", name+" "+args) // this is all right
+func Caller(command string, args []string) string {
+	// cmd := exec.Command(command, args) // this lead to error if args contains space
+	cmd := exec.Command(command, args...) // this is all right
 	if output, err := cmd.Output(); err != nil {
 		return ""
 	} else {
